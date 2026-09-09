4e0ffa66ed0d0f17559760ad3d3a959b0a4bdbf8
clarenvc <clarenvc@gmail.com>
2026-09-09 16:48:03 +0700

    Run web with Experience page

--- main/views.py
+++ main/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+from main.models import Experience
+
+
+def show_main(request):
+    context = {
+        "name": "Karen Lim",
+        "npm": "2506623982",
+        "study_program": "S1 Information Systems",
+        "bio": (
+            "2nd Year student of Universitas Indonesia's Information System"
+            " program. Currently pursuing my degree with interests in game"
+            "development and web development."
+        ),
+    }
+    return render(request, "index.html", context)
+
+
+def show_experience(request):
+    context = {
+        "name": "Karen Lim",
+        "experience_list": Experience.objects.all(),
+    }
+    return render(request, "experience.html", context)
